07e68f1844c47304d615e85d0037109c2e274867
Williedaniels <w.daniels@alustudent.com>
2024-06-30 19:29:31 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import F12Main from './F12Main';
+
+
+
+const router = createBrowserRouter([
+  { path: '/', element: <F12Main /> },
+]);
+
+export default function App() {
+  return (
+    <RouterProvider router={router} />
+  );
+}
